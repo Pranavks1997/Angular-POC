a76d004f314c3964488e4335cdcc3c2200ee208e
pranav_k <pranav_k@hashedin.com>
2020-01-09 21:52:46 +0530

    theme change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'POC';
+  constructor(private themeService: NbThemeService) {
+    this.themeService.changeTheme('default');
+  }
 }
