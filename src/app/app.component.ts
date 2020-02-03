66b89a6559dc4d2daf1982c32458c39a8e3ea322
pranav_k <pranav_k@hashedin.com>
2020-02-03 16:52:08 +0530

    few changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [DatePipe]
 })
 export class AppComponent {
   title = 'POC';
-  constructor(private themeService: NbThemeService) {
+  myDate = new Date();
+  constructor(private themeService: NbThemeService, private datePipe: DatePipe) {
     this.themeService.changeTheme('default');
   }
 }
